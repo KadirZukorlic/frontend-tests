f7f74340c3755d658ef078425be313fb8d67ecba
Kadir Zukorlic <kadirzukorlic@gmail.com>
2022-09-15 18:01:49 +0200

    Test transform to number func

--- src/util/numbers.test.js
+++ src/util/numbers.test.js
@@ -8,13 +8,23 @@ it('Should yield a value which is a typeof number', () => {
 
 	expect(result).toBeTypeOf('number')
 })
+it('Should yield a value which is a typeof number', () => {
+	const input = '2'
+
+	const result = transformToNumber(input)
+
+	expect(result).toBe(+input) // toBe(+input)
+})
 
 // it('Should yield NaN for a non-transformable values', () => {
 // 	const input = 'invalid'
+// 	const input2 = {}
 
 // 	const result = transformToNumber(input)
+// 	const result2 = transformToNumber(input2)
 
 // 	expect(result).toBeNaN()
+// 	expect(result2).toBeNaN()
 // })
 
 it('Should throw an error if a passed value is non-transformable', () => {
