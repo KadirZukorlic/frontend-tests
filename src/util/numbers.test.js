7f340738af966b817de7246a65337c3f60dff031
Kadir Zukorlic <kadirzukorlic@gmail.com>
2022-09-18 01:27:53 +0200

    Describe transformToNumber function

--- src/util/numbers.test.js
+++ src/util/numbers.test.js
@@ -1,38 +1,40 @@
-import { it, expect } from 'vitest'
+import { describe, it, expect } from 'vitest'
 import { transformToNumber } from './numbers.js'
 
-it('Should yield a value which is a typeof number', () => {
-	const input = '2'
+describe('transformToNumber()', () => {
+	it('Should yield a value which is a typeof number', () => {
+		const input = '2'
 
-	const result = transformToNumber(input)
+		const result = transformToNumber(input)
 
-	expect(result).toBeTypeOf('number')
-})
-it('Should yield a value which is a typeof number', () => {
-	const input = '2'
+		expect(result).toBeTypeOf('number')
+	})
+	it('Should yield a value which is a typeof number', () => {
+		const input = '2'
 
-	const result = transformToNumber(input)
+		const result = transformToNumber(input)
 
-	expect(result).toBe(+input) // toBe(+input)
-})
+		expect(result).toBe(+input) // toBe(+input)
+	})
 
-// it('Should yield NaN for a non-transformable values', () => {
-// 	const input = 'invalid'
-// 	const input2 = {}
+	// it('Should yield NaN for a non-transformable values', () => {
+	// 	const input = 'invalid'
+	// 	const input2 = {}
 
-// 	const result = transformToNumber(input)
-// 	const result2 = transformToNumber(input2)
+	// 	const result = transformToNumber(input)
+	// 	const result2 = transformToNumber(input2)
 
-// 	expect(result).toBeNaN()
-// 	expect(result2).toBeNaN()
-// })
+	// 	expect(result).toBeNaN()
+	// 	expect(result2).toBeNaN()
+	// })
 
-it('Should throw an error if a passed value is non-transformable', () => {
-	const input = 'Invalid'
+	it('Should throw an error if a passed value is non-transformable', () => {
+		const input = 'Invalid'
 
-	const resultFn = () => {
-		transformToNumber(input)
-	}
+		const resultFn = () => {
+			transformToNumber(input)
+		}
 
-	expect(resultFn).toThrowError()
+		expect(resultFn).toThrowError()
+	})
 })
